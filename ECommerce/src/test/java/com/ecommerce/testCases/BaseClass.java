0062e33ac226f09389ff25ab9e13ea9e5a748793
Hp <Hp@DESKTOP-8O8LP38>
2021-06-10 18:31:20 +0530

    Second

--- ECommerce/src/test/java/com/ecommerce/testCases/BaseClass.java
+++ ECommerce/src/test/java/com/ecommerce/testCases/BaseClass.java
@@ -1,21 +1,50 @@
 package com.ecommerce.testCases;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import com.ecommerce.utilities.ReadConfig;
 
 public class BaseClass {
-	public String baseURL="https://admin-demo.nopcommerce.com/";
-	public String username="admin@yourstore.com";
-	public String password="admin";
+	ReadConfig readconfig=new ReadConfig();
+	public String baseURL=readconfig.getApplicationURL();
+	public String username=readconfig.getUsername();
+	public String password=readconfig.getPassword();
 	public WebDriver driver;
+	public Logger logger;
+	
 	
 	@BeforeClass
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\eclipse-workspace\\test\\ECommerce\\Drivers\\chromedriver.exe");
+		
+		
+		
+		System.setProperty("webdriver.chrome.driver", readconfig.getChromePath());
 		driver=new ChromeDriver();
-	}
+		
+		
+		/*else if(br.equals("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver",readconfig.getFirefoxPath());
+			driver = new FirefoxDriver();
+		}
+		else if(br.equals("ie"))
+		{
+			System.setProperty("webdriver.ie.driver",readconfig.getIEPath());
+			driver = new InternetExplorerDriver();
+		}*/
+		
+		driver.get(baseURL);
+		logger=logger.getLogger("ecommerce");
+		PropertyConfigurator.configure("log4j.properties");
+		
+		
+		}
 	
 	@AfterClass
 	public void tearDown() {
--- ECommerce/src/test/java/com/ecommerce/testCases/TC_LoginPageTest_001.java
+++ ECommerce/src/test/java/com/ecommerce/testCases/TC_LoginPageTest_001.java
@@ -7,23 +7,29 @@ import org.testng.annotations.Test;
 
 import com.ecommerce.pageObjects.LoginPage;
 
-public class TC_LoginPage_001 extends BaseClass {
+public class TC_LoginPageTest_001 extends BaseClass {
 
 	
 	
 	@Test
 	public void loginTest() throws InterruptedException {
+		logger.info("-----------TC_LoginPageTest_001------------");
 		driver.get(baseURL);
 		Thread.sleep(1000);
 		LoginPage lp=new LoginPage(driver);
 		lp.setUserName(username);
+		logger.info("Username entered");
 		lp.setPassword(password);
+		logger.info("Password entered");
 		lp.clickLogin();
+		logger.info("login clicked");
 		
 	String title=driver.getTitle();
 	System.out.println(title);
 	Assert.assertEquals(title, "Dashboard / nopCommerce administration","Matches");
+	logger.info("Title matches");
 	lp.clickLogout();
+	logger.info("logged out");
 	/*if(title.equals("Dashboard / nopCommerce administration")) {
 		Assert.assertTrue(true);
 		lp.clickLogout();
