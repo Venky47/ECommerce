187e1c30f7e7c16efe8c49899d9787398eb693c5
Hp <Hp@DESKTOP-8O8LP38>
2021-06-11 14:47:36 +0530

    Third commit

--- ECommerce/src/test/java/com/ecommerce/testCases/TC_LoginPageTest_001.java
+++ ECommerce/src/test/java/com/ecommerce/testCases/TC_LoginPageTest_001.java
@@ -2,6 +2,9 @@ package com.ecommerce.testCases;
 
 
 
+import java.io.IOException;
+
+import org.openqa.selenium.Alert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,7 +15,7 @@ public class TC_LoginPageTest_001 extends BaseClass {
 	
 	
 	@Test
-	public void loginTest() throws InterruptedException {
+	public void loginTest() throws InterruptedException, IOException {
 		logger.info("-----------TC_LoginPageTest_001------------");
 		driver.get(baseURL);
 		Thread.sleep(1000);
@@ -23,18 +26,29 @@ public class TC_LoginPageTest_001 extends BaseClass {
 		logger.info("Password entered");
 		lp.clickLogin();
 		logger.info("login clicked");
+		Thread.sleep(2000);
 		
+		driver.switchTo().alert().accept();
 	String title=driver.getTitle();
 	System.out.println(title);
-	Assert.assertEquals(title, "Dashboard / nopCommerce administration","Matches");
+	/*Assert.assertEquals(title, "Dashboard / nopCommerce administration","Matches");
 	logger.info("Title matches");
-	lp.clickLogout();
-	logger.info("logged out");
-	/*if(title.equals("Dashboard / nopCommerce administration")) {
+	lp.clickLogout();*/
+	
+	if(title.equals("Your store. Login")) {
 		Assert.assertTrue(true);
 		lp.clickLogout();
-	}*/
+		logger.info("logged out");
+	}
+		else {
+			captureScreen(driver, "TC_LoginPageTest_001");
+			Assert.assertTrue(false);
+			lp.clickLogout();
+			logger.info("logged out");
+		}
+	
+	}
 	}
 	
 	
-}
+
